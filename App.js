ff30259ce4de477af43d10f547c1ca55d6356e3b
Julian Swerdlin <julianswer36@gmail.com>
2021-07-13 14:54:43 -0300

    last files

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-import UsersList from './screens/UsersList'
-import CreateUserScreen from './screens/CreateUserScreen'
-import UserDetailScreen from './screens/UserDetailScreen'
+import UsersList from "./screens/UsersList";
+import CreateUserScreen from "./screens/CreateUserScreen";
+import UserDetailScreen from "./screens/UserDetailScreen";
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="UsersList" component={UsersList} />
-      <Stack.Screen name="CreateUserScreen" component={CreateUserScreen} />
-      <Stack.Screen name="UserDetailScreen" component={UsersList} />
+      <Stack.Screen
+        name="UsersList"
+        component={UsersList}
+        options={{ title: "Users list" }}
+      />
+      <Stack.Screen
+        name="CreateUserScreen"
+        component={CreateUserScreen}
+        options={{ title: "Create a new user" }}
+      />
+      <Stack.Screen
+        name="UserDetailScreen"
+        component={UserDetailScreen}
+        options={{ title: "User Detail" }}
+      />
     </Stack.Navigator>
-  )
+  );
 }
 
 export default function App() {
@@ -31,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/UserDetailScreen.js
+++ screens/UserDetailScreen.js
@@ -1,13 +1,154 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+  Alert
+} from "react-native";
+import firebase from "../database/firebase";
 
 const UserDetailScreen = (props) => {
-    console.log(props.route.params.userId)
-    return (
-        <View>
-            <Text>User detail</Text>
-        </View>
-    )
-}
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    email: "",
+    phone: "",
+  });
 
-export default UserDetailScreen
+  const [loading, setLoading] = useState(true);
+
+  const getUserById = async (id) => {
+    const dbRef = firebase.db.collection("users").doc(id);
+    const doc = await dbRef.get();
+    const user = doc.data();
+    setUser({
+      ...user,
+      id: doc.id,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getUserById(props.route.params.userId);
+  }, []);
+
+  const initalState = {
+    name: "",
+    email: "",
+    phone: "",
+  };
+
+  const [state, setState] = useState(initalState);
+
+  const handleChangeText = (value, name) => {
+    setUser({ ...user, [name]: value });
+  };
+
+  const deleteUser = async () => {
+    const dbRef = firebase.db.collection('users').doc(props.route.params.userId);
+    await dbRef.delete();
+    props.navigation.navigate('UsersList');
+  }
+
+  const updateUser = async () => {
+    const dbRef = firebase.db.collection('users').doc(user.id);
+    await dbRef.set({
+        name: user.name,
+        email: user.email,
+        phone: user.phone
+    })
+    setUser(initalState)
+    props.navigation.navigate('UsersList');
+  }
+
+  const openConfirmationAlert = () => {
+    Alert.alert('Remove the User', 'Are you sure?', [
+        {text: 'Yes', onPress: () => deleteUser()},
+        {text: 'No', onPress: () => console.log('false')}
+    ])
+  }
+
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="#9e9e9e" />
+      </View>
+    );
+  }
+  return (
+    <ScrollView style={styles.container}>
+      {/* Name Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          placeholder="Name"
+          value={user.name}
+          onChangeText={(value) => handleChangeText(value, "name")}
+        />
+      </View>
+
+      {/* Email Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          value={user.email}
+          placeholder="Email"
+          multiline={true}
+          numberOfLines={4}
+          onChangeText={(value) => handleChangeText(value, "email")}
+        />
+      </View>
+
+      {/* Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          placeholder="phone"
+          value={user.phone}
+          onChangeText={(value) => handleChangeText(value, "phone")}
+        />
+      </View>
+
+      <View>
+        <Button
+          color="#19ac52"
+          title="Save User"
+          onPress={() => updateUser()}
+        />
+      </View>
+      <View>
+        <Button
+          color="#E37399"
+          title="Delete User"
+          onPress={() => openConfirmationAlert() }
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  loader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default UserDetailScreen;
